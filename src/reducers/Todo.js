6cd6221b81905f27ab371cf8a253a4b5c56be671
takehiko ogasawara <>
2019-04-05 22:18:14 +0900

    del 開発

---
+++ src/reducers/Todo.js
@@ -0,0 +1,25 @@
+import { Z_DEFAULT_STRATEGY } from "zlib";
+
+const initialState = {
+    todoList: []
+  }
+  
+  export const todoReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case 'ADD_TODO':
+        // 新しく追加するTODO
+        const todo = action.payload.todo;
+        // stateを複製して追加
+        const newState = Object.assign({}, state);
+        newState.todoList.push(todo);
+        return newState;
+      case 'DEL_TODO' :
+        const deltodo = action.payload.deltodo;
+        const delState = Object.assign({}, state);
+        delState.todoList.push(deltodo);
+        return delState;
+      default:
+        return state;
+    }
+  };
+
