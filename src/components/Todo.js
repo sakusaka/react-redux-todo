8f197ada35f02013c05f152d5116973256b82df9
jun68ykt <jun68.ykt@gmail.com>
2019-05-05 20:01:10 +0900

    配列要素にidを追加し、id指定による削除追加

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Todo.css';
 
 export default class Todo extends React.Component {
   state = {
@@ -6,14 +7,27 @@ export default class Todo extends React.Component {
   }
 
   render() {
+    const { addTodo, delTodo, delById } = this.props;
+
     // StoreのTodoからリストを生成
-    const list = this.props.todo.todoList.map((todo, index) => <li key={index}>{todo}</li>)
+    const list = this.props.todo.todoList.map(e =>
+      <li key={e.id}>
+        <button
+          className="delete"
+          onClick={() => delById(e.id)}
+        >
+          削除
+        </button>
+        <span className="todoId">{e.id}</span>
+        {e.todo}
+      </li>
+    );
 
     return (
       <div>
         <input type="text" onChange={elm => this.setState({ todo: elm.target.value })} />
-        <button onClick={() => this.props.addTodo(this.state.todo)}>追加</button>
-        <button onClick={() => this.props.delTodo(this.state.todo)}>削除</button>
+        <button onClick={() => addTodo(this.state.todo)}>追加</button>
+        <button onClick={() => delTodo(this.state.todo)}>削除</button>
         <ul>
           {list}
         </ul>
--- src/reducers/Todo.js
+++ src/reducers/Todo.js
@@ -1,23 +1,36 @@
 import { Z_DEFAULT_STRATEGY } from "zlib"; // eslint-disable-line
 
 const initialState = {
-    todoList: [],
-  }
-  
-  export const todoReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case 'ADD_TODO':
-        return {
-          ...state,
-          todoList: state.todoList.concat([action.payload.id, action.payload.todo])
-        };
-      case 'DEL_TODO':
-        return {
-          ...state,
-          todoList: state.todoList.filter(todo => todo !== action.payload.todo)
-        };
-      default:
-        return state;
-    }
-  };
+  todoList: [],
+};
 
+let nextTodoId = 9001;
+
+export const todoReducer = (state = initialState, action) => {
+  let index, nextTodoList;
+
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        ...state,
+        todoList: [...state.todoList, { id: nextTodoId ++, todo: action.payload.todo }]
+      };
+    case 'DEL_TODO':
+      return {
+        ...state,
+        todoList: state.todoList.filter(e => e.todo !== action.payload.todo)
+      };
+    case 'DEL_BY_ID':
+      nextTodoList = [...state.todoList];
+      index = state.todoList.findIndex(e => e.id === action.payload.id);
+      if (index >= 0) {
+        nextTodoList.splice(index, 1);
+      }
+      return {
+        ...state,
+        todoList: nextTodoList
+      };
+    default:
+      return state;
+  }
+};
